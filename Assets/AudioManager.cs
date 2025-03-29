fb20d88707d62a51d11db0e899e23591b5079b7b
Ru <raquelsilver21@gmail.com>
2025-03-29 15:25:56 +0100

    Grass Assets

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -6,19 +6,36 @@ public class AudioManager : MonoBehaviour
 {
     [Header("-------- Audio Source --------")]
     [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource ambientSource;
     [SerializeField] AudioSource SFXSource;
 
-
-    [Header ("-------- Audio Clip --------")]
+    [Header("-------- Audio Clip --------")]
     public AudioClip background;
+    public AudioClip ambient;
     public AudioClip walking;
     public AudioClip jump;
 
+    [Header("-------- Volume Settings --------")]
+    [Range(0f, 1f)] public float musicVolume = 1f;
+    [Range(0f, 1f)] public float ambientVolume = 1f;
+    [Range(0f, 1f)] public float sfxVolume = 1f;
 
     private void Start()
     {
+        
         musicSource.clip = background;
+        musicSource.volume = musicVolume;
+        musicSource.loop = true;
         musicSource.Play();
+
+        
+        ambientSource.clip = ambient;
+        ambientSource.volume = ambientVolume;
+        ambientSource.loop = true;
+        ambientSource.Play();
+
+        
+        SFXSource.volume = sfxVolume;
     }
 
     public void PlaySFX(AudioClip clip)
@@ -28,13 +45,19 @@ public class AudioManager : MonoBehaviour
 
     public void SetMusicVolume(float volume)
     {
-        musicSource.volume = volume;
+        musicVolume = volume;
+        musicSource.volume = musicVolume;
+    }
+
+    public void SetAmbientVolume(float volume)
+    {
+        ambientVolume = volume;
+        ambientSource.volume = ambientVolume;
     }
 
     public void SetSFXVolume(float volume)
     {
-        SFXSource.volume = volume;
+        sfxVolume = volume;
+        SFXSource.volume = sfxVolume;
     }
-
-
 }
