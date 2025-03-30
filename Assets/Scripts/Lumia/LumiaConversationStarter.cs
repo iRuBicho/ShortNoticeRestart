0d5c2ce42e82448b241c2c1d4d7a18b9bad20149
Ru <raquelsilver21@gmail.com>
2025-03-30 17:51:53 +0200

    Conflite Merge

--- Assets/Scripts/Lumia/LumiaConversationStarter.cs
+++ Assets/Scripts/Lumia/LumiaConversationStarter.cs
@@ -29,17 +29,11 @@ public class LumiaConversationStarter : MonoBehaviour
         }
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     public void OnSelectHaveItem()
     {
         Debug.Log("Player selected: I have the items.");
     }
 
-=======
->>>>>>> parent of 940e821 (UI Dialogue)
-=======
->>>>>>> parent of 940e821 (UI Dialogue)
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
