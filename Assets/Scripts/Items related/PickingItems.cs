6df09876ba945816d04b1bbfdde29a694af07646
Ru <raquelsilver21@gmail.com>
2025-05-20 18:41:41 +0200

    Back

--- Assets/Scripts/Items related/PickingItems.cs
+++ Assets/Scripts/Items related/PickingItems.cs
@@ -16,10 +16,6 @@ public class PickupItem : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInside = true;
-            if (GetComponent<ShowInteractionPrompt>() != null && !pickedUp)
-            {
-                GetComponent<ShowInteractionPrompt>().enabled = true;
-            }
         }
     }
 
@@ -28,10 +24,6 @@ public class PickupItem : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInside = false;
-            if (GetComponent<ShowInteractionPrompt>() != null)
-            {
-                GetComponent<ShowInteractionPrompt>().enabled = false;
-            }
         }
     }
 
@@ -42,10 +34,45 @@ public class PickupItem : MonoBehaviour
             onPickup.Invoke();
             pickedUp = true;
             GetComponent<Collider>().enabled = false;
-            if (GetComponent<ShowInteractionPrompt>() != null)
-            {
-                GetComponent<ShowInteractionPrompt>().enabled = false;
-            }
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}using UnityEngine;
+using PixelCrushers.QuestMachine;
+using UnityEngine.Events;
+
+public class PickupItem : MonoBehaviour
+{
+    public string questName;
+    public string counterName;
+    public KeyCode pickupKey = KeyCode.F;
+    public UnityEvent onPickup;
+    private bool playerInside = false;
+    private bool pickedUp = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (playerInside && !pickedUp && other.CompareTag("Player") && Input.GetKeyDown(pickupKey))
+        {
+            onPickup.Invoke();
+            pickedUp = true;
+            GetComponent<Collider>().enabled = false;
             Destroy(gameObject, 0.1f);
         }
     }
