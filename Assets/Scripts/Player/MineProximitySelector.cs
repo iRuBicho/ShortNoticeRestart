2f38707322858bbcc3934ba2d4bfef5027e4492c
Ru <raquelsilver21@gmail.com>
2025-05-22 15:11:57 +0200

    Prompt Text fixed
    Able to fix the Prompt Text showing when we get close to Lumia and the Items

---
+++ Assets/Scripts/Player/MineProximitySelector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class CustomProximitySelector : ProximitySelector
+{
+    private string displayMessage = "";
+
+    protected override void Update()
+    {
+        base.Update();
+
+        displayMessage = "";
+
+        if (currentUsable != null)
+        {
+            var targetSelector = currentUsable.GetComponent<ProximitySelector>();
+            if (targetSelector != null)
+            {
+                displayMessage = targetSelector.defaultUseMessage;
+            }
+            else
+            {
+                displayMessage = defaultUseMessage;
+            }
+        }
+    }
+
+    public override void OnGUI()
+    {
+        if (!string.IsNullOrEmpty(displayMessage))
+        {
+            var screenPos = new Rect(Screen.width / 2 - 100, Screen.height - 100, 200, 30);
+            GUIStyle style = new GUIStyle(GUI.skin.label)
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = 20,
+                normal = { textColor = Color.yellow }
+            };
+
+            GUI.Label(screenPos, displayMessage, style);
+        }
+    }
+}
