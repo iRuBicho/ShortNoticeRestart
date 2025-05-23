b6585a671b1802dcdd64a83bac3794be9a62f542
Ru <raquelsilver21@gmail.com>
2025-05-23 17:45:49 +0200

    Story Scene Added
    Story Scene added.This will be more polished, but gives the player some background.
    There is also a continue button

---
+++ Assets/StoryContinue.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryContinue : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game"); 
+    }
+}
